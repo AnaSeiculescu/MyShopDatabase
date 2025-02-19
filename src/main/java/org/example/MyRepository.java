0c0357d7024a3165f0cceab1410726114a2e5010
Ana Seiculescu <anaseiculescu@gmail.com>
2025-02-19 22:41:58 +0200

    connecting to the database and getting the orders

---
+++ src/main/java/org/example/MyRepository.java
@@ -0,0 +1,73 @@
+package org.example;
+
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyRepository {
+
+//	query a database through JDBC:
+//• load the JDBC driver (this is a class) - the class loader needs the class to be loaded
+//• establish a connection to the DBMS• create a statement
+//• execute it => obtain a ResultSet
+//• process the ResultSet Object
+//• close the connection
+
+	private Connection connection;
+
+	public void connect() {
+		String jdbcUrl = "jdbc:postgresql://localhost:3050/myshop";
+		String username = "postgres";
+		String password = "anadb";
+
+		// Register the PostmydatabasegreSQL driver
+
+		try {
+			Class.forName("org.postgresql.Driver");
+
+			// Connect to the database
+			connection = DriverManager.getConnection(jdbcUrl, username, password);
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		// Perform desired database operations
+
+		// Close the connection
+//		closeConnection();
+	}
+
+	private void closeConnection() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public List<Order> getAllOrders() {
+		List<Order> orderList = new ArrayList<>();
+		try {
+			connect();
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery("SELECT * FROM orders");
+			while (resultSet.next())
+			{
+				Order order = new Order();
+				int id = resultSet.getInt("id");
+				Date date = resultSet.getDate("date");
+				String address = resultSet.getString("address");
+				order.setAddress(address);
+				order.setDate(date);
+				order.setId(id);
+				orderList.add(order);
+
+			}
+
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		closeConnection();
+		return orderList;
+	}
+}
---
+++ src/main/java/org/example/Order.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.util.Date;
+
+public class Order {
+	private int id;
+	private Date date;
+	private String address;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+}
